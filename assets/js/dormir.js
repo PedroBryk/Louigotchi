924447792580515c94074e33b9c14d62dd3cfdd4
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2023-09-02 00:03:49 -0300

    Update dormir.js

--- assets/js/dormir.js
+++ assets/js/dormir.js
@@ -1,5 +1,7 @@
 var btn = document.querySelector('.action__sleep');
 
+var img = document.querySelector('.character');
+
 btn.addEventListener("click", function() {
 
     document.body.style.backgroundColor = "black";
