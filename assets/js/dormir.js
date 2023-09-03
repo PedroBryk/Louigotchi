0bb00d1b7dac069ebef778bec86252332b45c206
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2023-09-03 19:54:54 -0300

    criação de js para dormir

--- assets/js/dormir.js
+++ assets/js/dormir.js
@@ -1,8 +1,14 @@
 var btn = document.querySelector('.action__sleep');
 
+var ponto = document.querySelector('.ponto');
+
+console.log(ponto);
+
 btn.addEventListener("click", function() {
 
     document.body.style.backgroundColor = "black";
+
+    ponto.style.color = "white";
 
     var img = document.querySelector('.character');
 
