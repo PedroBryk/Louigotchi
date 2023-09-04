3b47ec28ba1335fd5dcab2076b45c81d5e1ad36b
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2023-09-04 19:38:42 -0300

    dormir js finalizado

--- assets/js/dormir.js
+++ assets/js/dormir.js
@@ -6,9 +6,24 @@ btn.addEventListener("click", function alternarCores() {
     paragrafos.forEach(paragrafo => {
         if (paragrafo.style.color === "black") {
             paragrafo.style.color = "white";
+            document.body.back
         } else {
             paragrafo.style.color = "black";
         }
     });
 });
+
+function toggleBackgroundColor() {
+    var body = document.body;
+    var currentColor = body.style.backgroundColor;
+
+    // Verifica se a cor atual é preto
+    if (currentColor === "black" || currentColor === "") {
+        body.style.backgroundColor = "#92F611";
+    } else {
+        body.style.backgroundColor = "black";
+    }
+}
+
+btn.addEventListener("click", toggleBackgroundColor);
 
